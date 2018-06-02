31c1c0a1a93f2ed2cca798f8a30e707be14288b5
Fabricio Rojas <fabriciorojascuomo@outlook.es>
2018-06-02 18:13:04 +0100

    Added some improvements

--- main.js
+++ main.js
@@ -1,36 +1,57 @@
-var header =  [
-    'First section',
-    'Second section',
-];
-var text =  [
-    'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.',
-    'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.',
-    'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.',
+var pageContent = [
+    {
+        'header': 'First section',
+        'text': ['1Lorem ipsum dolor sit amet consectetur adipiscing elit neque per sollicitudin taciti, viverra eros turpis rutrum magnis potenti tortor torquent dis posuere, tempus nam sodales fusce bibendum inceptos augue praesent feugiat vulputate. Molestie hendrerit cum placerat varius volutpat sem pellentesque vel nam ridiculus, sociis habitant fusce gravida nunc class faucibus dictumst eleifend, a commodo curae nostra tellus augue quisque rhoncus orci. Condimentum taciti metus himenaeos proin platea pulvinar litora cum nec, risus ridiculus imperdiet dis magna scelerisque sapien hac primis tempus, malesuada dignissim sollicitudin neque lectus nisl libero ante. Viverra mus ultricies blandit pretium eleifend eros sem aliquet, commodo ad platea per turpis ullamcorper. Tristique vulputate vel pretium odio aliquam fermentum interdum, proin dictumst habitasse fusce dis vehicula, pharetra inceptos nisi at cursus placerat. Phasellus dictumst blandit felis molestie vivamus tristique, class cras dui commodo nulla vehicula nibh, ultricies congue augue tempus bibendum. Nulla nibh et tincidunt taciti penatibus himenaeos convallis magnis, neque dapibus hendrerit justo diam etiam nullam, feugiat dui eleifend porta potenti rhoncus nisl.'],
+        'background':'',
+        'font':''
+    },
+    {
+        'header': 'Second section',
+        'text': ['2Lorem ipsum dolor sit amet consectetur adipiscing elit neque per sollicitudin taciti, viverra eros turpis rutrum magnis potenti tortor torquent dis posuere, tempus nam sodales fusce bibendum inceptos augue praesent feugiat vulputate. Molestie hendrerit cum placerat varius volutpat sem pellentesque vel nam ridiculus, sociis habitant fusce gravida nunc class faucibus dictumst eleifend, a commodo curae nostra tellus augue quisque rhoncus orci. Condimentum taciti metus himenaeos proin platea pulvinar litora cum nec, risus ridiculus imperdiet dis magna scelerisque sapien hac primis tempus, malesuada dignissim sollicitudin neque lectus nisl libero ante. Viverra mus ultricies blandit pretium eleifend eros sem aliquet, commodo ad platea per turpis ullamcorper. Tristique vulputate vel pretium odio aliquam fermentum interdum, proin dictumst habitasse fusce dis vehicula, pharetra inceptos nisi at cursus placerat. Phasellus dictumst blandit felis molestie vivamus tristique, class cras dui commodo nulla vehicula nibh, ultricies congue augue tempus bibendum. Nulla nibh et tincidunt taciti penatibus himenaeos convallis magnis, neque dapibus hendrerit justo diam etiam nullam, feugiat dui eleifend porta potenti rhoncus nisl.','Lorem ipsum dolor sit amet consectetur adipiscing elit neque per sollicitudin taciti, viverra eros turpis rutrum magnis potenti tortor torquent dis posuere, tempus nam sodales fusce bibendum inceptos augue praesent feugiat vulputate. Molestie hendrerit cum placerat varius volutpat sem pellentesque vel nam ridiculus, sociis habitant fusce gravida nunc class faucibus dictumst eleifend, a commodo curae nostra tellus augue quisque rhoncus orci. Condimentum taciti metus himenaeos proin platea pulvinar litora cum nec, risus ridiculus imperdiet dis magna scelerisque sapien hac primis tempus, malesuada dignissim sollicitudin neque lectus nisl libero ante. Viverra mus ultricies blandit pretium eleifend eros sem aliquet, commodo ad platea per turpis ullamcorper. Tristique vulputate vel pretium odio aliquam fermentum interdum, proin dictumst habitasse fusce dis vehicula, pharetra inceptos nisi at cursus placerat. Phasellus dictumst blandit felis molestie vivamus tristique, class cras dui commodo nulla vehicula nibh, ultricies congue augue tempus bibendum. Nulla nibh et tincidunt taciti penatibus himenaeos convallis magnis, neque dapibus hendrerit justo diam etiam nullam, feugiat dui eleifend porta potenti rhoncus nisl.'],
+        'background':'',
+        'font':'white'},
+    {
+        'header': 'Third section',
+        'text': ['3Lorem ipsum dolor sit amet consectetur adipiscing elit neque per sollicitudin taciti, viverra eros turpis rutrum magnis potenti tortor torquent dis posuere, tempus nam sodales fusce bibendum inceptos augue praesent feugiat vulputate. Molestie hendrerit cum placerat varius volutpat sem pellentesque vel nam ridiculus, sociis habitant fusce gravida nunc class faucibus dictumst eleifend, a commodo curae nostra tellus augue quisque rhoncus orci. Condimentum taciti metus himenaeos proin platea pulvinar litora cum nec, risus ridiculus imperdiet dis magna scelerisque sapien hac primis tempus, malesuada dignissim sollicitudin neque lectus nisl libero ante. Viverra mus ultricies blandit pretium eleifend eros sem aliquet, commodo ad platea per turpis ullamcorper. Tristique vulputate vel pretium odio aliquam fermentum interdum, proin dictumst habitasse fusce dis vehicula, pharetra inceptos nisi at cursus placerat. Phasellus dictumst blandit felis molestie vivamus tristique, class cras dui commodo nulla vehicula nibh, ultricies congue augue tempus bibendum. Nulla nibh et tincidunt taciti penatibus himenaeos convallis magnis, neque dapibus hendrerit justo diam etiam nullam, feugiat dui eleifend porta potenti rhoncus nisl.'],
+        'background':'',
+        'font':''}
 ];
 var mainTitle = "Page Title";
 
 window.addEventListener("DOMContentLoaded", function() {
-    document.title = mainTitle;
-    addElement('h1', 'title', mainTitle);
-    var topTabElements = ""
-    for (var i = 0; i < text.length; i++) {
-        var active = i<1?'class="active"':"";
-        var title = header[i] !== undefined ? '<div class="header" id="'+header[i].replace(/\s+/g, '').toLocaleLowerCase()+'"><h2>'+header[i]+'</h2><div class="underline"></div></div>' : "";
-        if (header[i] !== undefined) topTabElements += '<a href="#'+header[i].replace(/\s+/g, '').toLocaleLowerCase()+'"><li '+active+'>'+header[i]+'</li></a>'
-        addElement('div', 'content', title + '<div class="text"><p>'+text[i]+'</p></div>', i);
-    }   
-    addElement('ul', 'topTab', topTabElements);
-    addElement('a', 'footer', mainTitle);
-    document.getElementById("topTab").addEventListener("click", function(e) {
-        for(var i=0;i<this.getElementsByTagName("li").length;i++) this.getElementsByTagName("li")[i].classList.remove('active');
-        e.srcElement.classList.add('active');
-    }, false);
-    
+document.title = mainTitle;
+addElement('h1', 'title', mainTitle,'','');
+let topTabElements = ""
+for(let i=0;i<pageContent.length;i++){
+    let active = i<1?'class="active"':"";
+    let title = pageContent[i].header !== undefined ? '<div class="header" id="'+pageContent[i].header.replace(/\s+/g, '').toLocaleLowerCase()+'"><h2>'+pageContent[i].header+'</h2><div class="underline"></div></div>' : "";
+    topTabElements += '<a href="#'+pageContent[i].header.replace(/\s+/g, '').toLocaleLowerCase()+'"><li '+active+'>'+pageContent[i].header+'</li></a>'
+    for(let j=0;j<pageContent[i].text.length;j++){
+        title = j<1?title:"";
+        addElement('div', 'content', title + '<div class="text"><p>'+pageContent[i].text[j]+'</p></div>',pageContent[i].background,pageContent[i].font);
+    }
+}
+addElement('ul', 'topTab', topTabElements,'','');
+addElement('a', 'footer', mainTitle,'','');
+document.getElementById("topTab").addEventListener("click", function(e) {
+    for(let i=0;i<this.getElementsByTagName("li").length;i++) this.getElementsByTagName("li")[i].classList.remove('active');
+    e.srcElement.classList.add('active');
 }, false);
 
-function addElement(element, node, string){
-    var elm = document.createElement(element);
-    elm.innerHTML = string; 
-    if(node === "footer") elm.href = "#"
-    document.getElementById(node).appendChild(elm);
+}, false);
+
+function addElement(element,node,string,back,text){
+let elm = document.createElement(element);
+elm.innerHTML = string; 
+elm.style.padding = "0 50px 20px"; 
+elm = addElementStyle(elm, back, text)
+if(node === "footer") elm.href = "#"
+document.getElementById(node).appendChild(elm);
 }
+
+function addElementStyle(elm,back,text){
+if(back!='')elm.style.backgroundColor = back;
+if(text!='')elm.style.color = text;
+return elm;
+}
+
